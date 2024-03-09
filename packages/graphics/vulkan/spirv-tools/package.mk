215743ae3229ae5d924e017682ec14b8fb025f11
Rudi Heitbaum <rudi@heitbaum.com>
2024-03-09 08:06:22 +0000

    spirv-tools: update to githash 04896c4
    Supports build of glslang 14.1.0

--- packages/graphics/vulkan/spirv-tools/package.mk
+++ packages/graphics/vulkan/spirv-tools/package.mk
@@ -6,8 +6,8 @@ PKG_NAME="spirv-tools"
 # The SPIRV-Tools pkg_version needs to match the compatible (known_good) glslang pkg_version.
 # https://raw.githubusercontent.com/KhronosGroup/glslang/${PKG_VERSION}/known_good.json
 # When updating glslang pkg_version please update to the known_good spirv-tools pkg_version.
-PKG_VERSION="44d72a9b36702f093dd20815561a56778b2d181e"
-PKG_SHA256="f92a82ab3d04c1086fb412423acdf473e4462608761caecf9e50ed83e48dd725"
+PKG_VERSION="04896c462d9f3f504c99a4698605b6524af813c1"
+PKG_SHA256="c0d01e758a543b3a358cb97af02c6817ebd3f5ff13a2edf9fb220646a3d67999"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/SPIRV-Tools"
 PKG_URL="https://github.com/KhronosGroup/SPIRV-Tools/archive/${PKG_VERSION}.tar.gz"
